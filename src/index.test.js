b587c112450a96f05d84f7e6b697acabcb298783
krmenon <krmenon@adobe.com>
2020-01-09 21:52:28 -0800

    Feature 1 (#1)
    * add cci config
    
    * nui-release dry script

---
+++ src/index.test.js
@@ -0,0 +1,8 @@
+var avengers = require('./index');
+var expect = require('chai').expect;
+
+describe('avenger-names', function() {
+  it('should work!', function() {
+    expect(true).to.be.true;
+  })
+});
