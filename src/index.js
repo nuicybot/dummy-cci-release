ba4fe0138cd47dd048d7106733b46e5ee6d2613e
krmenon <krmenon@adobe.com>
2020-01-12 16:44:51 -0800

    BREAKING CHANGE: api changes Feature19 (#20)
    * BREAKING CHANGE: api changed to allNames
    
    * BREAKING CHANGE: changed api to randomNames

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ var avengerNames = require('./avenger-names.json');
 var ura = require('unique-random-array');
 
 module.exports = {
-  all: avengerNames,
-  random: ura(avengerNames)
+  allNames: avengerNames,
+  randomNames: ura(avengerNames)
 };
