01ce8e93196bb73a59b6a1ce39295aa8fa417e7f
krmenon <krmenon@adobe.com>
2020-01-14 14:20:57 -0800

    New: Feature42 (#43)
    * New: adding new hero to team
    
    * correcting spelling in name
    
    * Breaking: changing api

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ var avengerNames = require('./avenger-names.json');
 var ura = require('unique-random-array');
 
 module.exports = {
-  allNames: avengerNames,
-  random: ura(avengerNames)
+  all: avengerNames,
+  randomNames: ura(avengerNames)
 };
