3e43531eeee83f74a5c888a8c891a58949ba6cbd
krmenon <krmenon@adobe.com>
2020-01-17 17:00:22 -0800

    no-release: this is to test no release (#48)

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,11 @@
 var avengers = require('./index');
 var expect = require('chai').expect;
+const mm = require('micromatch');
 
 describe('avenger-names', function() {
   it('should work!', function() {
     expect(true).to.be.true;
+    console.log(mm.isMatch(' dont need a release to README 1 (#47)','!no-release:*'));
+    // console.log(mm.isMatch('no-release hello how are you', 'no-release*'))
   })
 });
