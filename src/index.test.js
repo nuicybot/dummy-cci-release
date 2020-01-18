9284f0dc6b8c0f2cca2864a8343a5aba1fcff0e4
krmenon <krmenon@adobe.com>
2020-01-17 17:11:26 -0800

    this should be just patch (#51)

--- src/index.test.js
+++ src/index.test.js
@@ -5,7 +5,7 @@ const mm = require('micromatch');
 describe('avenger-names', function() {
   it('should work!', function() {
     expect(true).to.be.true;
-    console.log(mm.isMatch(' dont need a release to README 1 (#47)','!no-release:*'));
+    // console.log(mm.isMatch(' dont need a release to README 1 (#47)','!no-release:*'));
     // console.log(mm.isMatch('no-release hello how are you', 'no-release*'))
   })
 });
